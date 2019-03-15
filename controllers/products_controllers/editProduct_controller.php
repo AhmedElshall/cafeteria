8142e6aae23de83ef180f748aeb1f2a94b828c54
AhmedElshall <ahmed.elshall93@gmail.com>
2019-03-15 20:32:42 +0200

    Merging Controllers with Views And Modles Of Products And Categories

--- controllers/products_controllers/editProduct_controller.php
+++ controllers/products_controllers/editProduct_controller.php
@@ -2,13 +2,12 @@
     require_once("../../models/products_model.php");
     require_once("../../models/category_model.php");
 
-   
+    
 
     class EditProduct{
-       
+
         function edit (){
 
-           
             $category= new Category ();
             $allCategories= $category->selectAll();
 
@@ -25,20 +24,22 @@
                 $image = isset($_POST['img']) ?  $_POST['img'] :NULL;
                 $price = isset($_POST['price']) ?  $_POST['price'] :NULL;
                 $status = isset($_POST['status']) ?  $_POST['status'] :NULL;
-                $categoryID = isset($_POST['category']) ?  $_POST['category'] :NULL;
+                $categoryID = isset($_POST['categoryID']) ?  $_POST['categoryID'] :NULL;
             
             }
         
             $product->updateProduct($id,$name,$image,$price,$status,$categoryID); 
-            header("Location: listProduct_controller.php");
+           header("Location: listProduct_controller.php");
             
         }
-       
+    
 
 
 }
 
 $product= new EditProduct();
 $product->edit();
+
+require_once('../../tempViews/10_edit-product.php');
 
 ?>
