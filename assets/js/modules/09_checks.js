7412bb1222b1524f54bd2769cfc0648ae0943218
mostafa <mostapha.a.aly@gmail.com>
2019-03-15 11:53:15 +0200

    my orders

--- assets/js/modules/09_checks.js
+++ assets/js/modules/09_checks.js
@@ -6,7 +6,7 @@ userRow.each(function() {
   $(this).on("click", ".user", function() {
     $(this)
       .next()
-      .slideToggle("slow");
+      .fadeToggle("700");
     $(this)
       .find("i")
       .toggleClass("fa-minus-square fa-plus-square", 1000);
@@ -17,7 +17,7 @@ userData.each(function() {
   $(this).on("click", ".user-data", function() {
     $(this)
       .next()
-      .slideToggle("700");
+      .fadeToggle("700");
     $(this)
       .find("i")
       .toggleClass("fa-minus-square fa-plus-square", 1000);
